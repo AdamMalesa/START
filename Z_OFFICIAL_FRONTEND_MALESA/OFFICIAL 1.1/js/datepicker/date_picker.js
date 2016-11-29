0c56c9fd255cb9aacb44d261daeb1c88d048fe55
Adam Master <malesa.adam@gmail.com>
2016-11-29 04:47:33 +0100

    STRAT_dodanie_plikow

---
+++ Z_OFFICIAL_FRONTEND_MALESA/OFFICIAL 1.1/js/datepicker/date_picker.js
@@ -0,0 +1,11 @@
+$('.kalendarz_okres').datepicker({
+   autoclose: true,
+  format: "mm/dd/yy",
+  maxViewMode: 0,
+  startDate: "now",
+  language: "pl"
+
+});
+$(".kalendarz_okres").on("changeDate", function(event) {
+
+});
---
+++ Z_OFFICIAL_FRONTEND_MALESA/OFFICIAL 1.1/js/slide_toggle_menu_mobile.js
@@ -0,0 +1,5 @@
+          $( ".navbar_menu_mobile" ).show();
+            $( ".toggle_button-collapse" ).click(function() {
+            $( ".navbar_menu_mobile" ).slideToggle( "slow", function() {
+            });
+            });
